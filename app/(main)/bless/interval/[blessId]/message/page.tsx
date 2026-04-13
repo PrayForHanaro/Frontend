b444defe7d0e4adcf365a36bf653cfd6a2a573b5
ksb <241875294+devsunb3-wq@users.noreply.github.com>
2026-04-13 17:14:22 +0900

    feat: add bless message compose page #7

--- app/(main)/bless/interval/[blessId]/message/page.tsx
+++ app/(main)/bless/interval/[blessId]/message/page.tsx
@@ -1,3 +1,31 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import BibleVerseBox from '../../../_components/bible-verse-box';
+import BlessActionButton from '../../../_components/bless-action-button';
+import MessageTextarea from '../../../_components/message-textarea';
+import { getRandomVerse } from '../../../_data/bible-verses';
+
 export default function BlessIntervalMessage() {
-  return <div>page</div>;
+  const router = useRouter();
+  const [message, setMessage] = useState('');
+  const [verse] = useState(getRandomVerse);
+
+  const handleNext = () => {
+    router.push('/bless/interval/complete');
+  };
+
+  return (
+    <div className="flex min-h-full flex-col bg-hana-white-yellow pb-20">
+      <div className="flex flex-col gap-6 px-6 pt-8">
+        <BibleVerseBox verse={verse} />
+        <MessageTextarea value={message} onChange={setMessage} />
+      </div>
+
+      <div className="mt-auto px-6 pt-8 pb-6">
+        <BlessActionButton onClick={handleNext}>다음 단계로</BlessActionButton>
+      </div>
+    </div>
+  );
 }
