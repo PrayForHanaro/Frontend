3cdf9c4a6d2a61d445d6230a93a24e94c5a8dc65
ksb <241875294+devsunb3-wq@users.noreply.github.com>
2026-04-14 16:53:17 +0900

    refactor: remove kakao share button from bless interval complete #7

--- app/(main)/bless/interval/complete/page.tsx
+++ app/(main)/bless/interval/complete/page.tsx
@@ -3,7 +3,6 @@
 import { useRouter } from 'next/navigation';
 import BlessActionButton from '../../_components/bless-action-button';
 import HeartLetterIcon from '../../_components/heart-letter-icon';
-import KakaoShareButton from '../../_components/kakao-share-button';
 
 export default function BlessIntervalComplete() {
   const router = useRouter();
@@ -22,7 +21,6 @@ export default function BlessIntervalComplete() {
       </div>
 
       <div className="flex flex-col gap-3 px-6 pb-10">
-        <KakaoShareButton />
         <BlessActionButton onClick={() => router.push('/bless/interval')}>
           완료
         </BlessActionButton>
