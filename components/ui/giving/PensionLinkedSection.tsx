8823c51fb6f995d15fbbb6eecda6e8d3586f1679
typeYu <mytypeyu@gmail.com>
2026-04-15 16:18:34 +0900

    feat: Section #18

--- components/ui/giving/PensionLinkedSection.tsx
+++ components/ui/giving/PensionLinkedSection.tsx
@@ -1,10 +1,14 @@
 'use client';
 
+import PensionLinkedView from './PensionLinkedView';
+
+/**
+ * @page: 연금 연계 섹션
+ * @description: 연금 연계 섹션 컴포넌트입니다. 헌금 관리 페이지가 추가 될 예정입니다.
+ * @author: typeYu
+ * @date: 2026-04-15
+ */
+
 export default function PensionLinkedSection() {
-  return (
-    <div className="flex flex-col gap-3">
-      <h2 className="font-hana-main text-[18px] text-hana-black">연금 연계</h2>
-      <div>연금 연계 섹션</div>
-    </div>
-  );
+  return <PensionLinkedView />;
 }
