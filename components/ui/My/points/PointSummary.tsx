e2766b8d32f59b39edde3ae258b385f36b652708
campus2 H056 <ryujy0716@gmail.com>
2026-04-17 22:58:19 +0900

    feat: 포인트 내역 페이지  수정 완료 #3

--- components/ui/My/points/PointSummary.tsx
+++ components/ui/My/points/PointSummary.tsx
@@ -5,9 +5,20 @@ type Props = {
 export default function PointSummary({ totalPoint }: Props) {
   return (
     <div className="flex flex-col items-center">
-      <img src="/pointgiving.svg" alt="point" className="mb-0 h-35 w-50" />
+      {/* 👇 그라데이션 영역 */}
+      <div className="relative flex items-center justify-center">
+        {/* 🌈 그라데이션 배경 */}
+        <div className="absolute h-40 w-40 rounded-full bg-gradient-to-r from-green-200 via-green-100 to-transparent blur-2xl" />
 
-      <span className="mt-0 font-extrabold text-3xl text-hana-main">
+        {/* 이미지 */}
+        <img
+          src="/pointgiving.svg"
+          alt="point"
+          className="relative z-10 h-35 w-50"
+        />
+      </div>
+
+      <span className="mt-2 font-extrabold text-3xl text-hana-main">
         {totalPoint.toLocaleString()}P
       </span>
     </div>
